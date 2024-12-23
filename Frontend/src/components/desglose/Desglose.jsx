d3d83432e8e2717db1193aac156be4e35812815e
Bryant <bryanttejeda628f@hotmail.com>
2024-12-23 17:03:10 -0400

    Working on sideBar

---
+++ Frontend/src/components/desglose/Desglose.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import "./Desglose.css"
+export default function Desglose() {
+    
+    return (
+        <div className="mainContainer">
+            <div className="sideBar">
+                
+            </div>
+            <div className="desgloseContainer">
+
+            </div>
+        </div>
+    )
+}
+
---
+++ Frontend/src/components/sidebar/SideBar.jsx
@@ -0,0 +1,15 @@
+import "./SideBar.css"
+
+export default function SideBar() {
+    
+    return (
+        <div className="sideBar">
+            <h1>Grupo <br />Pimentelense</h1>
+            <ul>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Desglose</a></li>
+                <li><a href="#">Cotizacion</a></li>
+            </ul>
+        </div>
+    )
+}
